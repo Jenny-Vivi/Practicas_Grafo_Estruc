f455820e8dbc98410dbdac2ee1e39d3adf295557
Viviana Carchi <jcarchif@est.ups.edu.ec>
2024-07-04 04:09:20 -0500

    Practica Grafos Proyecto

---
+++ bin/README.md
@@ -0,0 +1,14 @@
+
+El proyecto consta de 4 clases basadas en en clases realizadas.
+# User:
+el Implementar la clase User para representar a los usuarios de la red social.
+# Graph:
+Implementar la clase Graph para gestionar las conexiones entre usuarios.
+# RecommendationEngine
+Implementar la clase RecommendationEngine con algoritmos de similaridad para recomendar amigos.
+# Node Graph:
+el ella se implemento la lista generica de usuario y amigos
+
+
+# Realizado por:
+Viviana Carchi
---
+++ src/Main/Materia/Controllers/Graph.java
@@ -0,0 +1,32 @@
+package Main.Materia.Controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Main.Materia.Models.NodeGraph;
+
+public class Graph<T> {
+    private List<NodeGraph<T>> nodes;
+
+    public Graph() {
+        this.nodes = new ArrayList<>();
+    }
+
+    public void addNode(NodeGraph<T> node) {
+        nodes.add(node);
+    }
+
+    public void addConnection(NodeGraph<T> node1, NodeGraph<T> node2) {
+        node1.addFriend(node2.getUser());
+        node2.addFriend(node1.getUser());
+    }
+
+    public List<T> getFriends(NodeGraph<T> node) {
+        return node.getFriends();
+    }
+
+    public List<T> recommendFriends(NodeGraph<T> node) {
+        // Este método sería implementado por RecommendationEngine
+        return new ArrayList<>();
+    }
+}
